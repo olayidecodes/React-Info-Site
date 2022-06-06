60b6dfb88d4150b9feec6fbbd3833644f80002bc
olayidecodes <olayidecodes@gmail.com>
2022-06-06 23:46:18 +0100

    added new task

--- Part_1/index.js
+++ Part_1/index.js
@@ -1,8 +1,38 @@
-import React from "react"
-import ReactDOM from "react-dom"
+ 
+function Header() {
+    return(
+        <header>
+            <nav className="navbar">
+                <img src="react-logo.png" className="image"/>
+                <h3>ReactFacts</h3>
+                <h4>React Course - Project 1</h4>
+            </nav>
+        </header>
+    )
+}
+function MainContent(){
+    return(
+        <div className="main">
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise app, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
 
-const page = (
-    <h1>Testing</h1>
-)
+function Page(){
+    return(
+        <div>
+            <Header />
+            <MainContent />
+        </div>
+    )
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"))
+ 
